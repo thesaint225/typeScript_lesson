86543e125ec13a68e5294787decc216879714666
henrymiessan <miessanhenri@gmail.com>
2024-07-08 23:49:30 +0000

    working on the fuction on TypeScript

---
+++ myFunction.ts
@@ -0,0 +1,13 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+signUpUser("miessan", "johndoe@gmail.com", false);
+
+addTwo(5);
