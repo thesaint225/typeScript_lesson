bab8f85b04a6676e9f57affbd3b33daf57732684
henrymiessan <miessanhenri@gmail.com>
2024-07-11 15:21:59 +0000

    still learning type

--- myObject.ts
+++ myObject.ts
@@ -17,11 +17,46 @@
 
 // // type aliase
 
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+
+// function createUser(user: User):User {
+//   return { name: " ", email: " ", isActive: true };
+// }
+// createUser({ name: " ", email: " ", isActive: true });
+
+// keyword, questionmark
+
 type User = {
+  readonly _id: string;
   name: string;
   email: string;
   isActive: boolean;
+  creditCard?: number;
 };
 
-function createUser(user: User) {}
-createUser({ name: " ", email: " ", isActive: true });
+let myUser: User = {
+  _id: "12343",
+  name: "henri",
+  email: "miessanhenri@gmail.com",
+  isActive: false,
+};
+
+myUser.email = "henri@gmail.com";
+
+type cardNumber = {
+  cardnumber: string;
+};
+
+type cardDate = {
+  cardDate: string;
+};
+
+type cardDetails = cardNumber &
+  cardDate &
+  cardDate & {
+    cvv: number;
+  };
