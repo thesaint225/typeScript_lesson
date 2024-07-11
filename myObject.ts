4692c6bfdb48082943631fb31e74d37f59b24f7d
henrymiessan <miessanhenri@gmail.com>
2024-07-11 14:37:18 +0000

    learn type aliase

--- myObject.ts
+++ myObject.ts
@@ -1,16 +1,27 @@
-const User = {
-  name: "henri",
-  email: "miessanhenri@gmail.com",
-  isActive: true,
+// const User = {
+//   name: "henri",
+//   email: "miessanhenri@gmail.com",
+//   isActive: true,
+// };
+
+// function createUser({ name: string, isPaid: boolean }) {}
+
+// createUser({ name: "john", isPaid: true });
+
+// function createCourse(): { name: string; price: number } {
+//   return {
+//     name: "french",
+//     price: 399,
+//   };
+// }
+
+// // type aliase
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
 };
 
-function createUser({ name: string, isPaid: boolean }) {}
-
-createUser({ name: "john", isPaid: true });
-
-function createCourse(): { name: string; price: number } {
-  return {
-    name: "french",
-    price: 399,
-  };
-}
+function createUser(user: User) {}
+createUser({ name: " ", email: " ", isActive: true });
