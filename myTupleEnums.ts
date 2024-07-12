e378110a9626cb5f78a6ad65f62279dbf105f809
henrymiessan <miessanhenri@gmail.com>
2024-07-12 08:15:21 +0000

    learn enum

--- myTupleEnums.ts
+++ myTupleEnums.ts
@@ -6,4 +6,4 @@ let rgb: [number, number, number] = [255, 123, 112];
 
 type User = [number, string];
 
-const newUser: User = [112, "miessanhenri@gmail.com"];
+const newUser: User = [112, "miessanhenri@gmail.com "];
