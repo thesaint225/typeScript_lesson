a5308c445f8dcecea4476959eb8a0365b22b37ef
henrymiessan <miessanhenri@gmail.com>
2024-07-12 07:53:25 +0000

    tuple

---
+++ myTupleEnums.ts
@@ -0,0 +1,9 @@
+let tUser: [string, number, boolean];
+
+tUser = ["hen", 434, true];
+
+let rgb: [number, number, number] = [255, 123, 112];
+
+type User = [number, string];
+
+const newUser: User = [112, "miessanhenri@gmail.com"];
--- myunion.ts
+++ myunion.ts
@@ -1,21 +1,25 @@
-let score: number | string = 33;
+// let score: number | string = 33;
 
-score = "55";
+// score = "55";
 
-type User = {
-  name: string;
-  id: number;
-};
+// type User = {
+//   name: string;
+//   id: number;
+// };
 
-type Admin = {
-  userName: string;
-  id: number;
-};
+// type Admin = {
+//   userName: string;
+//   id: number;
+// };
 
-let henri: User | Admin = { name: "hrnri", id: 54433 };
-henri = { userName: "jon", id: 343 };
+// let henri: User | Admin = { name: "hrnri", id: 54433 };
+// henri = { userName: "jon", id: 343 };
 
-// array
-const data: number[] = [1, 2, 3];
-const data1: string[] = ["1", "2", "3"];
-const data2: string | number[] = [1, 2, 3, "4"];
+// // array
+// const data: number[] = [1, 2, 3];
+// const data1: string[] = ["1", "2", "3"];
+// const data2: (string | number)[] = [1, 2, 3, "4"];
+
+// let seatAllotment: "aisile"| "middle"|"window"
+
+// // tuples
